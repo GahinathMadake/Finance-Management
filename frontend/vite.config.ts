8e0359f7db52a464b61157984453f6a9e5a1fc85
GahinathMadake <gahinathmadake@gmail.com>
2025-07-06 15:48:39 +0530

    Resest Back

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // Add these essential settings:
-  base: '/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
 })
