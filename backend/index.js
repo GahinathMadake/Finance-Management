0bb2b02dc5f9b76e3b4ee357e9abb590ea6a04e1
GahinathMadake <gahinathmadake@gmail.com>
2025-07-06 14:23:29 +0530

    Backend is Live

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173', // local dev frontend
+      'https://finance-management-rpoy.onrender.com' // deployed frontend
+    ],
     credentials: true,
   })
 );
